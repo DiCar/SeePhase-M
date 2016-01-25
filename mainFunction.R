f48c842cf3265536f80939763584933c6fe74201
DiCar <DiCar@Paul-DiCarmines-MacBook-Pro.local>
2016-01-24 20:31:40 -0800

    Revert "Moved require package calls to top of file."
    This reverts commit 10e563bda48672c740dbc0312ad921115093d27b.

--- mainFunction.R
+++ mainFunction.R
@@ -1,40 +1,34 @@
 #main file
 
-#import required packages (PMD)
+
+
+#CO2 work
+
 source('helperFunctions.R')
-require(reshape2)
-require(ggplot2)
-require(manipulate) #allows the creation of an interactive plot
 
-#Define global variables (PMD)
-t <- Sys.time()
-timeStamp <-  strftime(t,"%Y-%m-%d_%H-%M-%S")
+foo <- readLines('settings.txt')
 
-# The Script
-settings.lines <- readLines('settings.txt') #reads the lines of the settings file into a character vector (PMD)
-
-for (i in 1:length(settings.lines)){
-  temp <- gsub("\\","/",settings.lines[i],fixed=TRUE)
+for (i in 1:length(foo)){
+  temp <- gsub("\\","/",foo[i],fixed=TRUE)
   eval(parse(text=temp))
 }
 
-#load VDS file into a list, vds
-vds <- read.delim(vdsFilePath)
+#load VDS file
+df <- read.delim(vdsFilePath) #tab delim format
 
-#set number of columns to save from VDS file
-vds <- vds[1:num_columns_keep_vds] #removes columns beyond vds.num.columns.keep, see settings file (PMD)
-vds <- vds[!is.na(vds[1]),] #removes empty cells, which are imported as NA (PMD)
+#set number of columns to save
+df <- df[1:num_columns_keep_vds] 
+df <- df[!is.na(df[1]),]
 
-elapsed.time <- genElapsedTime(vdsFilePath) #stores elapsed time of VDS method as a double, using helper function genElapsedTime (PMD)
-#line below can be moved to helperfunction genElapsedTime
-elapsed.time <- elapsed.time[!is.na(elapsed.time)] #removes blank cells from elapsed.time (PMD)
+elapsed.time <- genElapsedTime(vdsFilePath)
+elapsed.time <- elapsed.time[!is.na(elapsed.time)]
 
-vds2 <- cbind(vds, elapsed.time) #creates a new list, vds2, with elapsed time as a column (PMD)
+df2 <- cbind(df, elapsed.time)
 
-colnames(vds2)[rh_vds] = "rh" #changes RH column name from 'RH....' to 'rh' in vds2 (PMD)
+colnames(df2)[rh_vds] = "rh"
 
-#read in the SeePhase data (as a list) (PMD)
-seePhase <- read.csv(dataFilePath)
+#read in the CO2 data
+data1 <- read.csv(dataFilePath)
 
 ##humidity when working
 #data12 <- read.delim("Part 1 humidity.txt", stringsAsFactors = FALSE)
@@ -42,54 +36,56 @@ seePhase <- read.csv(dataFilePath)
 #data13 <- fix_length(data12, nrow(data1))
 
 
-elapsedTimeSec <- convertToSec(seePhase)
+elapsedTimeSec <- convertToSec(data1)
 elapsedTimeHour <- elapsedTimeSec / 3600
-#seePhase2 <- cbind(data1,elapsedTimeSec,elapsedTimeHour, data13)
-seePhase2 <- cbind(seePhase, elapsedTimeSec, elapsedTimeHour) #adds elapsedTimeSec and elapsedTimeHour columns to SeePhase data (PMD)
+#data2 <- cbind(data1,elapsedTimeSec,elapsedTimeHour, data13)
+data2 <- cbind(data1,elapsedTimeSec,elapsedTimeHour)
 
 #create names for slicing later
-name.seePhase.time = colnames(seePhase2)[seePhase.time] #assigns name from time column to variable; column is assigned by seePhase.time variable in settings file (PMD)
-name.seePhase.temperature = colnames(seePhase2)[seePhase.temperature] #assigns name from temperature column to variable; column is assigned by seePhase.temperature variable in settings file (PMD)
-name.seePhase.phase = colnames(seePhase2)[seePhase.phase] #assigns name from phase column to variable; column is assigned by seePhase.phase variable in settings file (PMD)
-name.vds.rh = colnames(vds2)[vds.rh] #should return 'rh' (PMD)
-name.vds.temperature = colnames(vds2)[vds.temperature] #should return 'Temp' (PMD)
-name.vds.cCO2 = colnames(vds2)[vds.cCO2] #returns 'Concentration.of.Gas...'(PMD)
-name.vds.cO2 = colnames(vds2)[vds.cO2] #returns 'elapsed.time'...should return O2 concentration (PMD)
-name.vds.time = colnames(vds2)[vds.time] #returns 'time.s.' (PMD)
-name.vds.include = colnames(vds2)[vds.include] #returns 'Include' (PMD)
+n_time_data = colnames(data2)[time_data]
+n_temp_data = colnames(data2)[temp_data]
+n_phase_data = colnames(data2)[phase_data]
 
-#Generate a plot from the raw SeePhase data
+n_rh_vds = colnames(df2)[rh_vds]
+n_temp_vds = colnames(df2)[temp_vds]
+n_cCO2_vds = colnames(df2)[cCO2_vds]
+n_cO2_vds = colnames(df2)[cO2_vds]
+n_time_vds = colnames(df2)[time_vds]
+n_include_vds = colnames(df2)[include_vds]
+
 createPlot <- function(x,y,xmin,xmax,ymin,ymax,offset_value,phase_data,include_column){
-  data35 <- extracting_points_offset(seePhase2, elapsed.time, offset_value)
+  data35 <- extracting_points_offset(data2, elapsed.time, offset_value)
   
   plot(x,y,type='l',xlim=c(xmin,xmax),ylim=c(ymin,ymax))
-  points(data35$elapsedTimeHour,data35[,seePhase.phase],col="red")
-  data37 <- data35[vds2[,include_column] == 0,]
-  points(data37$elapsedTimeHour,data37[,seePhase.phase],col="green")
+  points(data35$elapsedTimeHour,data35[,phase_data],col="red")
+  data37 <- data35[df2[,include_column] == 0,]
+  points(data37$elapsedTimeHour,data37[,phase_data],col="green")
 }
+
+
+require(manipulate)
 
 offset_value_main = 0
 
-#Generate the interactive plot for finding an acceptable offset value
 manipulate(
-  {createPlot(seePhase2$elapsedTimeHour,seePhase2[,seePhase.phase],x.min,x.max,y.min,y.max,offset_value,seePhase.phase,include_vds)
+  {createPlot(data2$elapsedTimeHour,data2[,phase_data],x.min,x.max,y.min,y.max,offset_value,phase_data,include_vds)
     offset_value_main <<- offset_value},
-  x.min = slider(0,as.integer(seePhase2$elapsedTimeHour[length(seePhase2$elapsedTimeHour)]),initial=0),
-  x.max = slider(0,as.integer(seePhase2$elapsedTimeHour[length(seePhase2$elapsedTimeHour)]),initial=as.integer(seePhase2$elapsedTimeHour[length(seePhase2$elapsedTimeHour)])),
-  y.min = slider(0,as.integer(max(seePhase2[,seePhase.phase]))+10,initial=0),
-  y.max = slider(0,as.integer(max(seePhase2[,seePhase.phase]))+10,initial=as.integer(max(seePhase2[,seePhase.phase]))+10),
+  x.min = slider(0,as.integer(data2$elapsedTimeHour[length(data2$elapsedTimeHour)]),initial=0),
+  x.max = slider(0,as.integer(data2$elapsedTimeHour[length(data2$elapsedTimeHour)]),initial=as.integer(data2$elapsedTimeHour[length(data2$elapsedTimeHour)])),
+  y.min = slider(0,as.integer(max(data2[,phase_data]))+10,initial=0),
+  y.max = slider(0,as.integer(max(data2[,phase_data]))+10,initial=as.integer(max(data2[,phase_data]))+10),
   offset_value = slider(-3600,3600,initial=0)
   
 )
 
-#Opportunity to stop the script if an acceptable offset value cannot be found
+
 cat ("Press [enter] to continue, 1 = stop")
 line <- readline()
 if (line == '1'){stop("user stopped script")}
 
 print(offset_value_main)
-data35 <- extracting_points_offset(seePhase2, elapsed.time, offset_value_main)
-data36 <- cbind(data35,vds2) #combines SeePhase and VDS data into one list (PMD)
+data35 <- extracting_points_offset(data2, elapsed.time, offset_value_main)
+data36 <- cbind(data35,df2)
 
 
 #comparing the results to those from sigmaplot
@@ -102,38 +98,44 @@ data36 <- cbind(data35,vds2) #combines SeePhase and VDS data into one list (PMD)
 
 
 #tf2 <- data36[c("Ph4","AnalogB","include","temp","rh", "Concentration.of.Gas....")]
-tf2 <- data36[c(name.seePhase.phase,name.seePhase.temperature,name.vds.include,name.vds.temperature,name.vds.rh,name.vds.cCO2)]
+tf2 <- data36[c(n_phase_data,n_temp_data,n_include_vds,n_temp_vds,n_rh_vds,n_cCO2_vds)]
 
-#Assign the column number of name.vds.include to a variable
-include.index <- grep(name.vds.include,colnames(tf2))
+include_index <- grep(n_include_vds,colnames(tf2))
 
-#Assign a character string to the interest.variable
-interest.variable <- colnames(tf2)[6]
-
-#Remove any incomplete data (is this necessary?, likely caused problems for Marvin or wouldn't be in the code)
+colnames(tf2)[6] = interest_variable
 tf3 <- tf2[complete.cases(tf2),]
 #tf4 <- tf3[tf3$include > 0,]
 #tf4 <- tf3[tf3$include > 1,]
 
-#Determine which rows to keep
-if (include.check == 0){
-  #Keep only rows where include.index value is > 0
-  tf4 <- tf3[tf3[,include.index] > 0,]
+
+if (include_check == 0){
+  tf4 <- tf3[tf3[,include_index] > 0,]
 }
-if (include.check > 1){
-  #Keep only rows where include.index value == include.check value in settings file
-  tf4 <- tf3[tf3[,include.index] == include.check,]
+if (include_check > 1){
+  tf4 <- tf3[tf3[,include_index] == include_check,]
+  
 }
 
-#Assign the column number of name.vds.temperature to a variable
-temp.index <- grep(name.vds.temperature,colnames(tf2))
 
-#create directory to store output files, and define the path to that directory
-dir.name <- paste(timeStamp, name.seePhase.phase)
+
+temp_index <- grep(n_temp_vds,colnames(tf2))
+
+
+
+require(reshape2)
+require(ggplot2)
+
+t <- Sys.time()
+timeStamp <-  strftime(t,"%Y-%m-%d_%H-%M-%S")
+
+dir.name <- paste(timeStamp, n_phase_data)
 dir.create(dir.name)
+
 tPath <- paste0(dir.name,"/")
 
-#runs this code if CO2 experiement (Haven't worked on this yet, PMD)
+
+
+
 if (process_type == '1'){
   e1<- split(tf4, tf4[,temp_index])
   
@@ -148,9 +150,9 @@ if (process_type == '1'){
     print(i)
     
     x_text = "table_check <- dcast(e1[[i]], AAAA1111 ~ BBBB2222, length, value.var='CCCC3333')"
-    x_text = sub("AAAA1111",interest.variable,x_text)
-    x_text = sub("BBBB2222",name.vds.rh,x_text)
-    x_text = sub("CCCC3333",name.seePhase.phase,x_text)
+    x_text = sub("AAAA1111",interest_variable,x_text)
+    x_text = sub("BBBB2222",n_rh_vds,x_text)
+    x_text = sub("CCCC3333",n_phase_data,x_text)
     #table_check <- dcast(e1[[i]], concentration.co2 ~ rh, length, value.var="Ph4")
     eval(parse(text=x_text))
     
@@ -158,9 +160,9 @@ if (process_type == '1'){
     write.csv(table_check,paste0(ttPath,"check_table",names(e1)[i],".csv"),row.names=FALSE)
     
     x_text2 = "table_averaged <- dcast(e1[[i]], AAAA1111 ~ BBBB2222, mean, value.var='CCCC3333')"
-    x_text2 = sub("AAAA1111",interest.variable,x_text2)
-    x_text2 = sub("BBBB2222",name.vds.rh,x_text2)
-    x_text2 = sub("CCCC3333",name.seePhase.phase,x_text2)
+    x_text2 = sub("AAAA1111",interest_variable,x_text2)
+    x_text2 = sub("BBBB2222",n_rh_vds,x_text2)
+    x_text2 = sub("CCCC3333",n_phase_data,x_text2)
     #table_averaged <- dcast(e1[[i]], concentration.co2 ~ rh, mean, value.var="Ph4")
     eval(parse(text=x_text2))
     
@@ -171,10 +173,10 @@ if (process_type == '1'){
     write("---------",file=paste0(tPath,"table_averaged.csv"),append=TRUE)
     write.table(table_averaged,paste0(tPath,"table_averaged.csv"),sep=",",append=TRUE,row.names=FALSE)
     
-    t_plot <- melt(table_averaged, id.vars = interest.variable, variable.name = 'series')
+    t_plot <- melt(table_averaged, id.vars = interest_variable, variable.name = 'series')
     number <- names(e1)[i]
     plotId <- paste0("CO2 vs RH vs Temperature Plot for ",number)
-    g <- ggplot(t_plot, aes_string(interest.variable,"value")) + geom_point(aes(colour = series)) + ggtitle(plotId)+xlab('CO2 Concentration')+ylab('Phase')
+    g <- ggplot(t_plot, aes_string(interest_variable,"value")) + geom_point(aes(colour = series)) + ggtitle(plotId)+xlab('CO2 Concentration')+ylab('Phase')
     g
     print(g)
     ggplotname <- paste0(ttPath,"plot_forTemperature_",names(e1)[i],".png")
@@ -186,11 +188,14 @@ if (process_type == '1'){
   write(paste0("offset_value_main = ",offset_value_main," #used for this set of run"),file=paste0(tPath,"settings.txt"),append=TRUE)
   write(paste0("process_type = ",process_type," #used for this set of run"),file=paste0(tPath,"settings.txt"),append=TRUE)
   write.csv(tf4,paste0(tPath,"selectedpoints.csv"),col.names=FALSE)
-  write.csv(seePhase2,paste0(tPath,"rawdata.csv"),col.names=FALSE)
+  write.csv(data2,paste0(tPath,"rawdata.csv"),col.names=FALSE)
 }
 
-if (process_type == '2'){
 
+
+if (process_type == '2'){
+  
+  
   #e1<- split(tf4, tf4[,temp_index])
   
   #write("TablePlot",file=paste0(tPath,"table_averaged.csv"))
@@ -201,20 +206,20 @@ if (process_type == '2'){
   
   
   x_text = "table_check <- dcast(tf4, AAAA1111 ~ BBBB2222, length, value.var='CCCC3333')"
-  x_text = sub("AAAA1111",name.vds.rh,x_text)
-  x_text = sub("BBBB2222",name.vds.temperature,x_text)
-  x_text = sub("CCCC3333",name.seePhase.phase,x_text)
+  x_text = sub("AAAA1111",n_rh_vds,x_text)
+  x_text = sub("BBBB2222",n_temp_vds,x_text)
+  x_text = sub("CCCC3333",n_phase_data,x_text)
   #table_check <- dcast(e1[[i]], concentration.co2 ~ rh, length, value.var="Ph4")
   #table_check <- dcast(tf4, rh ~ temp, length, value.var="Ph4")
   eval(parse(text=x_text))
   
-  print(table_check) #allows user to verify that the correct data is being used
+  print(table_check)
   write.csv(table_check,paste0(tPath,"check_table_RH.csv"),row.names=FALSE)
   
   x_text2 = "table_averaged <- dcast(tf4, AAAA1111 ~ BBBB2222, mean, value.var='CCCC3333')"
-  x_text2 = sub("AAAA1111",name.vds.rh,x_text2)
-  x_text2 = sub("BBBB2222",name.vds.temperature,x_text2)
-  x_text2 = sub("CCCC3333",name.seePhase.phase,x_text2)
+  x_text2 = sub("AAAA1111",n_rh_vds,x_text2)
+  x_text2 = sub("BBBB2222",n_temp_vds,x_text2)
+  x_text2 = sub("CCCC3333",n_phase_data,x_text2)
   #table_averaged <- dcast(e1[[i]], concentration.co2 ~ rh, mean, value.var="Ph4")
   #table_averaged <- dcast(tf4, rh ~ temp, mean, value.var="Ph4")
   eval(parse(text=x_text2))
@@ -226,10 +231,11 @@ if (process_type == '2'){
 #   write("---------",file=paste0(tPath,"table_averaged.csv"),append=TRUE)
 #   write.table(table_averaged,paste0(tPath,"table_averaged.csv"),sep=",",append=TRUE,row.names=FALSE)
   
-  t_plot <- melt(table_averaged, id.vars = name.vds.rh, variable.name = 'series')
+  t_plot <- melt(table_averaged, id.vars = n_rh_vds, variable.name = 'series')
   #number <- names(e1)[i]
-  plotId <- paste(name.seePhase.phase, "RH vs Temperature Plot")
-  g <- ggplot(t_plot, aes_string(name.vds.rh,"value")) + geom_point(aes(colour = series)) + ggtitle(plotId)+xlab('RH%')+ylab(name.seePhase.phase)
+  plotId <- paste(n_phase_data, "RH vs Temperature Plot")
+  g <- ggplot(t_plot, aes_string(n_rh_vds,"value")) + geom_point(aes(colour = series)) + ggtitle(plotId)+xlab('RH%')+ylab(n_phase_data)
+  g
   print(g)
   ggplotname <- paste0(tPath,"plot_forRHvsT.png")
   ggsave(file=ggplotname)
@@ -241,7 +247,7 @@ if (process_type == '2'){
   write(paste0("offset_value_main = ",offset_value_main," #used for this set of run"),file=paste0(tPath,"settings.txt"),append=TRUE)
   write(paste0("process_type = ",process_type," #used for this set of run"),file=paste0(tPath,"settings.txt"),append=TRUE)
   write.csv(tf4,paste0(tPath,"selectedpoints.csv"),col.names=FALSE)
-  write.csv(seePhase2,paste0(tPath,"rawdata.csv"),col.names=FALSE)
+  write.csv(data2,paste0(tPath,"rawdata.csv"),col.names=FALSE)
   
   
   
